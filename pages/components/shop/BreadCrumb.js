ad9823198f910ceb6d0156a6381ee60cdd88a8d1
susant <gracysusant@gmail.com>
2021-02-16 02:16:40 +0545

    breadcrumb change

--- pages/components/shop/BreadCrumb.js
+++ pages/components/shop/BreadCrumb.js
@@ -6,8 +6,8 @@ export default class BreadCrumb extends Component {
             <nav aria-label="breadcrumb" className="breadcrumb-nav">
                 <ol className="breadcrumb">
                     <li className="breadcrumb-item"><a href="index.html"><i className="icon-home" /></a></li>
-                    <li className="breadcrumb-item"><a href="#">Men</a></li>
-                    <li className="breadcrumb-item active" aria-current="page">Accessories</li>
+                    <li className="breadcrumb-item"><a href="#">Musical Instrument</a></li>
+                    <li className="breadcrumb-item active" aria-current="page">Singing Bowl</li>
                 </ol>
             </nav>
         )
