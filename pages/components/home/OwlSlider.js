21ad74e500f93e6b1585c89db298883ec2599e7f
susant <gracysusant@gmail.com>
2021-03-02 08:16:44 +0545

    carousel work

--- pages/components/home/OwlSlider.js
+++ pages/components/home/OwlSlider.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
-import 'pure-react-carousel/dist/react-carousel.es.css';
-import { SingleItemComponent } from '../ResuableComponent/SingleItemComponent/SingleItemComponent';
- 
-const OwlSlider = ({product}) => {
+import React, { useState, useEffect } from "react";
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+} from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
+import { SingleItemComponent } from "../ResuableComponent/SingleItemComponent/SingleItemComponent";
 
-    return (
-      <CarouselProvider
-        naturalSlideWidth={100}
-        naturalSlideHeight={125}
-        totalSlides={3}
-      >
-        <Slider>
-          {product.map(product => 
-            <Slide index={product.id}> <SingleItemComponent product={product}/> </Slide>
-          ) }
-         
-         
-        </Slider>
-      </CarouselProvider>
-    );
-  }
-export default OwlSlider
+const OwlSlider = ({ products }) => {
+  const [visibleSlides, setvisibleSlides] = useState(4);
+  const [slideHeight, setSlideHeight] = useState(320);
+
+  const reportWindowSize = (e) => {
+    // setvisibleSlides(3);
+    console.log(e.target.innerWidth);
+    const innerWidth = e.target.innerWidth;
+    if (innerWidth < 800) {
+      setvisibleSlides(2);
+      setSlideHeight(280);
+    } else {
+      setvisibleSlides(4);
+      // setSlideHeight(320);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("resize", reportWindowSize);
+  }, [reportWindowSize]);
+
+  return (
+    <CarouselProvider
+      visibleSlides={visibleSlides}
+      totalSlides={6}
+      step={3}
+      naturalSlideWidth={200}
+      naturalSlideHeight={slideHeight}
+    >
+      <Slider>
+        {products.map((product) => (
+          <Slide index={product.id}>
+            <SingleItemComponent product={product} />
+            {/* <p>{product.id}</p> */}
+          </Slide>
+        ))}
+      </Slider>
+    </CarouselProvider>
+  );
+};
+export default OwlSlider;
--- pages/components/home/indexFeaturedSection.js
+++ pages/components/home/indexFeaturedSection.js
@@ -1,58 +1,20 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import {SingleItemComponent} from '../ResuableComponent/SingleItemComponent/SingleItemComponent'
-import  OwlSlider  from "./OwlSlider";
+import { SingleItemComponent } from "../ResuableComponent/SingleItemComponent/SingleItemComponent";
+import OwlSlider from "./OwlSlider";
 
 function IndexFeaturedSection() {
- const {featuredProduct} = useSelector((state) => state.featuredProduct)
+  const { featuredProduct } = useSelector((state) => state.featuredProduct);
 
- console.log("featuredproduct", featuredProduct);
-  
+  console.log("featuredproduct", featuredProduct);
+
   return (
     <section className="featured-products-section">
       <div className="container">
         <h2 className="section-title heading-border ls-20 border-0">
           Featured Products
         </h2>
-        <OwlSlider product={featuredProduct} />
-        {/* <div
-          className="products-slider custom-products owl-carousel owl-theme nav-outer show-nav-hover nav-image-center owl-loaded owl-drag"
-          data-owl-options="{
-						'dots': false,
-						'nav': true
-					}"
-        >
-          <div className="owl-stage-outer">
-            <div
-              className="owl-stage"
-              style={{
-                transform: "translate3d(0px, 0px, 0px)",
-                transition: "all 0s ease 0s",
-                width: "2400px",
-              }}
-            >
-            {featuredProduct.map(product => 
-             
-                <SingleItemComponent key={product.id}  product={product} />
-            )}
-              
-              </div>
-            </div>
-          <div className="owl-nav">
-            <button
-              type="button"
-              role="presentation"
-              className="owl-prev disabled"
-            >
-              <i className="icon-angle-left" />
-            </button>
-            <button type="button" role="presentation" className="owl-next">
-              <i className="icon-angle-right" />
-            </button>
-          </div>
-          <div className="owl-dots disabled" />
-        </div> */}
-        {/* End .featured-proucts */}
+        <OwlSlider products={featuredProduct} />
       </div>
     </section>
   );
